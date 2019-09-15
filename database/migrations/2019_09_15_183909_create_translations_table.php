e54614547234d1c691a4cd43510314050e79e5d9
Ivan Khuda <ivanchuda@gmail.com>
2019-09-15 23:08:15 +0300

    setup app

---
+++ database/migrations/2019_09_15_183909_create_translations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('from');
+            $table->text('to');
+            $table->string('lang');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
